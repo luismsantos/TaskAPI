d161488abff89243d9dbd0e04a64f98bc36ddfa5
Luis Miguel <luismiguelsantossouza@gmail.com>
2025-01-31 16:26:08 -0300

    update Insert User

---
+++ TaskAPI/DTO/UserInsertDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskAPI.DTO
+{
+    public class UserInsertDTO
+    {
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+    }
+}
