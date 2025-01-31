42ab06e02160514e5c596ef05502e8911d62c747
Luis Miguel <luismiguelsantossouza@gmail.com>
2025-01-31 16:25:39 -0300

    edit User Validation

--- TaskAPI/Models/User.cs
+++ TaskAPI/Models/User.cs
@@ -4,11 +4,33 @@ namespace TaskAPI.Models
 {
     public class User
     {
-        [Key]
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public IEnumerable<Task> Tasks { get; set; }
+        [Key] public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        public IList<Task> Tasks { get; set; }
+
+
+        public void SetName(String name)
+        {
+            Name = name;
+        }
+
+        public void SetEmail(String email)
+        {
+            Email = email;
+        }
+
+        public void SetPassword(String password)
+        {
+            Password = password;
+        }
+
+        public User(String name, String email, String password)
+        {
+            SetName(name);
+            SetEmail(email);
+            SetPassword(password);
+        }
     }
 }
